0adc1a311e9edd10d6cae6417597f3d850f62813
bswieczak <bartlomiej.swieczak@gmail.com>
2019-07-11 19:37:06 +0200

    Add profiles dev and prod

--- src/main/java/com/example/demo/PetDispositorApplication.java
+++ src/main/java/com/example/demo/PetDispositorApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PetDispositorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(PetDispositorApplication.class, args);
     }
-
 }
